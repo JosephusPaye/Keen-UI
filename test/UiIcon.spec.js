cdd0b79b92c246ca6e826cb6af8ad8dc05b91d73
Josephus Paye II <j.paye96@gmail.com>
2016-04-16 09:45:47 +1000

    Rename disableText prop to removeText in UiIcon test

--- test/UiIcon.spec.js
+++ test/UiIcon.spec.js
@@ -4,14 +4,14 @@ import UiIcon from '../src/UiIcon.vue';
 describe('UiIcon.vue', function() {
     it('should initialize with correct data/props', function() {
         const vm = new Vue({
-            template: '<div><ui-icon icon="mail" :disable-text="true" v-ref:icon></ui-icon></div>',
+            template: '<div><ui-icon icon="mail" :remove-text="true" v-ref:icon></ui-icon></div>',
             components: {
                 UiIcon
             }
         }).$mount();
 
         expect(vm.$refs.icon.icon).toBe('mail');
-        expect(vm.$refs.icon.disableText).toBe(true);
+        expect(vm.$refs.icon.removeText).toBe(true);
     });
 
     it('should add icon prop as a class', function() {
@@ -36,9 +36,9 @@ describe('UiIcon.vue', function() {
         expect(vm.$refs.icon.$el.textContent).toBe('mail');
     });
 
-    it('should not render icon prop as inner text when disableText prop is true', function() {
+    it('should not render icon prop as inner text when removeText prop is true', function() {
         const vm = new Vue({
-            template: '<div><ui-icon icon="mail" :disable-text="true" v-ref:icon></ui-icon></div>',
+            template: '<div><ui-icon icon="mail" :remove-text="true" v-ref:icon></ui-icon></div>',
             components: {
                 UiIcon
             }
