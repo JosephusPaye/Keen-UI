40df17591b0eee95d5f89e0e3e3f7fd74b068d80
Josephus Paye II <j.paye96@gmail.com>
2017-01-31 11:58:17 +1100

    Allow for inspection in production mode

--- src-docs/main.js
+++ src-docs/main.js
@@ -1,6 +1,10 @@
 import '../src/helpers/modality';
 
 import Vue from 'vue';
+
+// Allow devtools inspection, even in production mode
+Vue.config.devtools = true;
+
 import App from './App.vue';
 
 new Vue({
