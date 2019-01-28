63ca60d4499f2e35c9dd0f2c2cde985eb7e0d24a
Josephus Paye II <j.paye96@gmail.com>
2019-01-28 20:30:19 +1100

    Add production build script, bump version in build

--- build/options.js
+++ build/options.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // Hardcoding here (and not reading from package.json) as the files are built
 // before the version is updated in package.json
-const version = '1.0.1';
+const version = '1.1.0';
 
 const banner =
   '/*!\n' +
