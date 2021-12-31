f3aebbf85fd7d44e0e7be5a0b66bb153b981dbe8
Sergiu C <kilobyte2007@gmail.com>
2021-12-31 05:07:27 +0200

    Hide UiCollapsible content overflow only during animation (#519)

--- src/UiCollapsible.vue
+++ src/UiCollapsible.vue
@@ -217,8 +217,12 @@ $ui-collapsible-header-background-hover     : $md-grey-300 !default;
 
 .ui-collapsible__body-wrapper {
     max-height: 0;
-    overflow: hidden;
     transition: max-height 0.3s ease;
+
+    &.v-enter-active,
+    &.v-leave-active {
+        overflow: hidden;
+    }
 }
 
 .ui-collapsible__body {
