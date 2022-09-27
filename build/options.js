e4e90a3b9e9482d95ef4eb1cc57c8168936ad30e
Sergiu Cazac <kilobyte2007@gmail.com>
2022-09-28 00:58:23 +0300

    Implemented the building with vite (rollup) and got rid of unnecessary deps (typo fix)

--- build/options.js
+++ build/options.js
@@ -1,4 +1,4 @@
- path = require('path');
+const path = require('path');
 
 // Hardcoding here (and not reading from package.json) as the files are built
 // before the version is updated in package.json
