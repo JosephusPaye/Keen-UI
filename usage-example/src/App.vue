a60212d9775cebf806fdb4661a5f3625e555bb8c
Josephus Paye II <j.paye96@gmail.com>
2023-01-20 21:57:17 +1100

    Temporarily comment out examples that aren't working

--- usage-example/src/App.vue
+++ usage-example/src/App.vue
@@ -1,15 +1,20 @@
 <script setup>
 import UsesDefaultExport from './components/UsesDefaultExport.vue';
-import UsesLibExport from './components/UsesLibExport.vue';
+// import UsesLibExport from './components/UsesLibExport.vue';
 import UsesNamedExport from './components/UsesNamedExport.vue';
-import UsesSrcExport from './components/UsesSrcExport.vue';
+// import UsesSrcExport from './components/UsesSrcExport.vue';
 </script>
 
 <template>
   <div>
+    <p style="color: #fff">The following should be styled Keen UI button components if this is working:</p>
+    <br />
     <UsesDefaultExport />
-    <UsesLibExport />
+    <!-- <br /> -->
+    <!-- <UsesLibExport /> -->
+    <br />
     <UsesNamedExport />
-    <UsesSrcExport />
+    <!-- <br /> -->
+    <!-- <UsesSrcExport /> -->
   </div>
 </template>
