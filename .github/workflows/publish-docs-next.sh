a497491fbd6348f99f95ea5a5af605d3b7b725c4
Josephus Paye II <j.paye96@gmail.com>
2023-04-07 15:09:57 +1000

    Fix publish docs script

--- .github/workflows/publish-docs-next.sh
+++ .github/workflows/publish-docs-next.sh
@@ -1,8 +1,11 @@
-# Exit on first error, treat unset variables as errors, and print commands as they're executed
-set -eux
+# Exit on first error, treat unset variables as errors
+set -eu
 
 NEXT_BRANCH="paye/auto-publish-docs" # TODO: Change this to `next` before merging
 GH_PAGES_BRANCH="gh-pages"
+
+echo "NEXT_BRANCH: $NEXT_BRANCH"
+echo "GH_PAGES_BRANCH: $GH_PAGES_BRANCH"
 
 # Read --push flag
 DO_PUSH=false
@@ -11,6 +14,8 @@ for arg in "$@"; do
     DO_PUSH=true
   fi
 done
+
+echo "DO_PUSH: $DO_PUSH"
 
 # Exit if the current branch is not `next`
 if [[ $(git rev-parse --abbrev-ref HEAD) != "$NEXT_BRANCH" ]]; then
@@ -21,24 +26,39 @@ fi
 
 # Keep track of the current commit sha
 NEXT_LAST_COMMIT=$(git rev-parse --short HEAD)
+echo "NEXT_LAST_COMMIT: $NEXT_LAST_COMMIT"
 
-# Build the docs
+echo ""
+echo "Building docs..."
 yarn build:docs
 
-# Switch to the `gh-pages` branch
+echo ""
+echo "Switching to the $GH_PAGES_BRANCH branch..."
 git checkout $GH_PAGES_BRANCH
 
-# Replace the existing next docs with a copy of the new docs
+echo ""
+echo "Replacing the existing \`next\` docs with a copy of the new docs..."
 rm -rf next && cp -r docs next
 
-# Commit the changes to the `next` folder
-if [[ -n $(git status --porcelain) ]]; then
-  git add next
-  git -c user.name="Josephus Paye II" -c user.email="j.paye96@gmail.com" \
-    commit -m "Add \`next\` docs for commit $NEXT_LAST_COMMIT"
+# Exit if there are no changes to the `next` directory
+HAS_NEXT_DIR_CHANGES=$(git diff --quiet --exit-code next)
+if [[ $HAS_NEXT_DIR_CHANGES == 0 ]]; then
+  echo "No changes to built \`next\` docs, skipping publish."
+  exit 0
 fi
+
+echo ""
+echo "Committing the changes..."
+git add next
+git -c user.name="Josephus Paye II" -c user.email="j.paye96@gmail.com" \
+  commit -m "Add \`next\` docs for commit $NEXT_LAST_COMMIT"
 
 # Push the changes to the `gh-pages` branch
 if [[ $DO_PUSH == true ]]; then
+  echo ""
+  echo "Pushing the changes to the $GH_PAGES_BRANCH branch..."
   git push origin $GH_PAGES_BRANCH
+else
+  echo ""
+  echo "Skipping push to the $GH_PAGES_BRANCH branch, use the --push flag to push."
 fi
